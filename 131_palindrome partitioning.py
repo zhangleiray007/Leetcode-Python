bf41de2ec13d54ab3e5058de21fd5136987f2c71
Lei Zhang <zhangleiray007@163.com>
2019-07-29 16:25:10 +0800

    Add files via upload

--- 131_palindrome partitioning.py
+++ 131_palindrome partitioning.py
@@ -8,3 +8,40 @@ class Solution:
                     for each in dp[j]:
                         dp[i].append([s[i:j]] + each)
         return dp[0]
+		
+		
+class Solution:
+    def partition(self, s):
+        res = []
+        path =[]
+        self.dfs(s, path,res,0,1)
+        return res
+    
+    def dfs(self, s,path,res,pre,start):
+        if start == len(s):
+            if s[pre:start] == s[pre:start][::-1]:
+                path.append(s[pre: start])
+                res.append(path.copy())
+                path.pop()
+            return
+        self.dfs(s,path,res,pre,start+1)
+        if s[pre: start] == s[pre: start][::-1]:
+            path.append(s[pre: start])
+            self.dfs(s,path,res,start,start+1)
+            path.pop()
+            
+			
+class Solution:
+    def partition(self, s):
+        if not s:
+            return [[]]
+        self.res =[]
+        self.dfs(s,[])
+        return self.res
+    
+    def dfs(self,s,path):
+        if len(s)==0:
+            self.res.append(path)
+        for i in range(len(s)):
+            if s[:i+1] == s[:i+1][::-1]:
+                self.dfs(s[i+1:],path+[s[:i+1]])
