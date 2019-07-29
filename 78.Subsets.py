8bdf5510531ac723f41c300a4c715af9421ac9c1
Lei Zhang <zhangleiray007@163.com>
2019-07-29 16:24:43 +0800

    Add files via upload

---
+++ 78.Subsets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        if n == 0 : return res
+        nums.sort()
+        selected = [False] * n
+        self.subset(nums, selected, 0, res)
+        return res
+    
+    def subset(self, nums, selected, step, res):
+        if step == len(nums):
+            sub = []
+            for i in range(len(nums)):
+                if selected[i] :
+                    sub.append(nums[i])
+            res.append(sub)
+            return
+        selected[step]=False
+        self.subset(nums,selected,step+1,res)
+        selected[step]=True
+        self.subset(nums,selected,step+1,res)
